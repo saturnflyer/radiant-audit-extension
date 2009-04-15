a7f3108fbda67f3710748111858a4ac033b926d3
Bennett Kolasinski <bkolasinski@bennett.local>
2009-04-15 13:31:33 -0400

    login/out spec

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Admin::WelcomeController do
+  dataset :users
+  
+  it "should log a login" do
+    count = AuditEvent.count
+    post :login, :user => {:login => "admin", :password => "password"}
+    AuditEvent.count.should == count+1
+  end
+  
+  it "should log a logout" do
+    post :login, :user => {:login => "admin", :password => "password"}
+    count = AuditEvent.count
+    get :logout
+    AuditEvent.count.should > count+1 #logout also updates a user's session token
+  end
+  
+end
