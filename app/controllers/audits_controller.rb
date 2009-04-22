40ab688ee91892fc9bb9483a1938952184d5abaf
Bennett Kolasinski <bkolasinski@bennett.local>
2009-04-22 13:58:06 -0400

    continue controller development-- waiting for sphinxable_resource to continue

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -15,10 +15,37 @@ class AuditsController < ApplicationController
     @next_date = @next_date.created_at unless @next_date.nil?
     @previous_date = @previous_date.created_at unless @previous_date.nil?
 
-    @audits = AuditEvent.find(:all, :conditions => ["created_at >= ? AND created_at < ?", @startdate, @enddate], :order => "created_at DESC")
+    # filter by date
+    conditions = "created_at > :startdate AND created_at < :enddate"
+    conditionshash = {:startdate => @startdate, :enddate => @enddate}
+
+    # filter by IP Address
+    if !params[:ip_address].blank?
+      conditions += " AND ip_address = :ip_address"
+      conditionshash[:ip_address] = params[:ip_address]
+    end
+
+    # filter by User ID
+    if !params[:user_id].blank?
+      conditions += " AND user_id = :user_id"
+      conditionshash[:user_id] = params[:user_id]
+    end
+
+    # filter by Event Type
+    if !params[:event_type].blank?
+      # event type comes through as "AUDITABLETYPE AUDITTYPE"; need to find both
+      auditable_type, audit_type = params[:event_type].split(" ")
+      audit_type_id = AuditType.find_by_name(audit_type).id
+      conditions += " AND auditable_type = :auditable_type AND audit_type_id = :audit_type_id"
+      conditionshash[:audit_type_id] = audit_type_id
+      # convert "AUDITABLETYPE" => "Auditabletype"- it's a class name
+      conditionshash[:auditable_type] = auditable_type.capitalize
+    end
+
+    @audits = AuditEvent.find(:all, :conditions => [conditions, conditionshash], :order => "created_at DESC")
     # some helper arrays for filter options
     @ip_addresses = @audits.map(&:ip_address).uniq
-    @users = @audits.map(&:user).uniq.map(&:name)
+    @users = @audits.map(&:user).uniq
     @event_types = @audits.collect { |a| a.event_type }.uniq
   end
 end
