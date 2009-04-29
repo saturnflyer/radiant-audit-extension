0d4d7c95c1dc5f6aab4dc2ade2fed42ef010ec2e
Bennett Kolasinski <bkolasinski@bennett.local>
2009-04-29 17:14:52 -0400

    custom report - initial create

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -15,8 +15,9 @@ class AuditsController < ApplicationController
     # => Message Text (sphinx)
     # pagination (max 100 / PP)
     # default order by date
-    @startdate = params[:startdate].nil? ? Date.today : Date.parse(params[:startdate])
-    @enddate = params[:enddate].nil? ? @startdate.next : Date.parse(params[:enddate])
+    @startdate = params[:startdate].blank? ? Date.today : Date.parse(params[:startdate])
+    # make enddate inclusive- show events through the enddate specified
+    @enddate = params[:enddate].blank? ? @startdate.next : Date.parse(params[:enddate]).next
     @next_date = AuditEvent.find(:first, :select => :created_at, :conditions => ["created_at > ?", @enddate], :order => "created_at ASC")
     @previous_date = AuditEvent.find(:first, :select => :created_at, :conditions => ["created_at < ?", @startdate], :order => "created_at DESC")
     @next_date = @next_date.created_at unless @next_date.nil?
