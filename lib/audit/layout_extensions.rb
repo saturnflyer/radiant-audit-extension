2aa22270a801f3237415276a8fe4a574fdea7e13
Bennett Kolasinski <bkolasinski@bennett.local>
2009-05-04 17:28:37 -0400

    audit parts of stuff

--- lib/audit/layout_extensions.rb
+++ lib/audit/layout_extensions.rb
@@ -5,12 +5,17 @@ module Audit
       base.class_eval do
         extend Auditable
         
+        updatables = ["name", "content"]
+        
         audit_event :create do |event|
           "#{event.user_link} created " + link_to(event.auditable.name, event.auditable_path)
         end
 
         audit_event :update do |event|
-          "#{event.user_link} updated " + link_to(event.auditable.name, event.auditable_path)
+          # take the intersection of any interesting fields that may have been updated 
+          updated = (event.auditable.changed & updatables).join(",") + " for " unless (event.auditable.changed & updatables).empty?
+          
+          "#{event.user_link} updated #{updated}" + link_to(event.auditable.name, event.auditable_path)
         end
 
         audit_event :destroy do |event|
--- lib/audit/snippet_extensions.rb
+++ lib/audit/snippet_extensions.rb
@@ -5,11 +5,15 @@ module Audit
       base.class_eval do
         extend Auditable
 
+        updatables = ["name", "content"]
+
         audit_event :create do |event|
           "#{event.user_link} created " + link_to(event.auditable.name, event.auditable_path)
         end
 
         audit_event :update do |event|
+          # take the intersection of any interesting fields that may have been updated 
+          updated = (event.auditable.changed & updatables).join(",") + " for " unless (event.auditable.changed & updatables).empty?
           "#{event.user_link} updated " + link_to(event.auditable.name, event.auditable_path)
         end
 
