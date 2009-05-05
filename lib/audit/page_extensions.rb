8980264709f0eb53ed47bf26353c9e6519b74351
Bennett Kolasinski <bkolasinski@bennett.local>
2009-05-05 12:26:24 -0400

    log page state changes

--- lib/audit/page_extensions.rb
+++ lib/audit/page_extensions.rb
@@ -14,18 +14,15 @@ module Audit
 
         audit_event :update do |event|
           if event.auditable.status_id_changed?
-            case event.auditable.status.name
-            when 'Published' : 
-              audit :item => event.auditable, :user => event.user, :ip => event.ip_address, :type => :publish
-            end
+            audit :item => event.auditable, :user => event.user, :ip => event.ip_address, :type => :status_change
           end
-          # take the intersection of any interesting fields that may have been updated 
+          # take the intersection of any interesting fields that may have been updated
           updated = (event.auditable.changed & updatables).join(", ") + " for" unless (event.auditable.changed & updatables).empty?
           "#{event.user_link} updated #{updated} " + link_to(event.auditable.title, event.auditable_path)
         end
 
-        audit_event :publish do |event|
-          "#{event.user_link} published " + link_to(event.auditable.title, event.auditable_path)
+        audit_event :status_change do |event|
+          "#{event.user_link} changed the status of " + link_to(event.auditable.title, event.auditable_path) + " to #{event.auditable.status.name}"
         end
 
         audit_event :destroy do |event|
