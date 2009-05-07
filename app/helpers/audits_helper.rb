8e10f84a13f6b673cff52e9b6f910deafa9f4ba3
Bennett Kolasinski <bennett@bennettk.com>
2009-05-07 14:04:13 -0400

    only show 'remove filter' if a filter's set

--- app/helpers/audits_helper.rb
+++ app/helpers/audits_helper.rb
@@ -1,2 +1,11 @@
 module AuditsHelper
+  def browse_by_date_filters_set?
+    # have any filters been set on the browse_by_date form?
+    !(params[:filter][:ip_address].blank? && params[:filter][:user_id].blank? && params[:event_type].blank? && params[:query].blank?)
+  end
+  
+  def custom_report_filters_set?
+    # have any filters been set on the custom_report form?
+    !(params[:startdate].blank? && params[:enddate].blank? && params[:filter][:ip_address].blank? && params[:filter][:user_id].blank? && params[:filter][:auditable_type].blank? && params[:filter][:auditable_id].blank? && params[:filter][:audit_type_name].blank? && params[:query].blank?)
+  end
 end
