599058840af8f16e449710ddff22a4220d7c519d
Bennett Kolasinski <bennett@bennettk.com>
2009-05-14 10:47:52 -0400

    spoo ,sdrawkcab nward gnieb sFERH

--- lib/audit/snippet_extensions.rb
+++ lib/audit/snippet_extensions.rb
@@ -6,20 +6,20 @@ module Audit
         extend Auditable
 
         audit_event :create do |event|
-          "#{event.user_link} created <a href='#{event.auditable.name}'>#{event.auditable_path}</a>"
+          "#{event.user_link} created <a href='#{event.auditable_path}'>#{event.auditable.name}</a>"
         end
 
         audit_event :update do |event|
           # we are interested in the following fields to see if they've changed
           # it will be noted in the log message if any of the following fields have changed
           updatables = ["name", "content"]
-          log_message = "#{event.user_link} updated <a href='#{event.auditable.name}'>#{event.auditable_path}</a>"
+          log_message = "#{event.user_link} updated <a href='#{event.auditable_path}'>#{event.auditable.name}</a>"
           log_message += " (#{(event.auditable.changed & updatables).join(", ")})" unless (event.auditable.changed & updatables).empty?
           log_message
         end
 
         audit_event :destroy do |event|
-          "#{event.user_link} deleted <a href='#{event.auditable.name}'>#{event.auditable_path}</a>"
+          "#{event.user_link} deleted <a href='#{event.auditable_path}'>#{event.auditable.name}</a>"
         end
       end
     end
--- lib/audit/user_extensions.rb
+++ lib/audit/user_extensions.rb
@@ -6,20 +6,20 @@ module Audit
         extend Auditable
 
         audit_event :create do |event|
-          "#{event.user_link} created <a href='#{event.auditable.login}'>#{event.auditable_path}</a>"
+          "#{event.user_link} created <a href='#{event.auditable_path}'>#{event.auditable.login}</a>"
         end
 
         audit_event :update do |event|
           # we are interested in the following fields to see if they've changed
           # it will be noted in the log message if any of the following fields have changed
           updatables = ["name", "email", "login", "password", "admin", "developer", "notes"]
-          log_message = "#{event.user_link} updated <a href='#{event.auditable.login}'>#{event.auditable_path}</a>"
+          log_message = "#{event.user_link} updated <a href='#{event.auditable_path}'>#{event.auditable.login}</a>"
           log_message += " (#{(event.auditable.changed & updatables).join(", ")})" unless (event.auditable.changed & updatables).empty?
           log_message
         end
 
         audit_event :destroy do |event|
-          "#{event.user_link} deleted <a href='#{event.auditable.login}'>#{event.auditable_path}</a>"
+          "#{event.user_link} deleted <a href='#{event.auditable_path}'>#{event.auditable.login}</a>"
         end
 
         audit_event :login do |event|
