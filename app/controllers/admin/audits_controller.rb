142c98aa3a746d70cb437e59151509dfc2fd4163
Josh French <josh@digitalpulp.com>
2009-06-10 18:26:12 -0400

    Cruft

--- app/controllers/admin/audits_controller.rb
+++ app/controllers/admin/audits_controller.rb
@@ -40,51 +40,5 @@ class Admin::AuditsController < ApplicationController
     def sort_direction
       params[:direction] == 'asc' ? 'asc' : 'desc'
     end
-
-    def prepare_sphinx_results
-      # in case no filter params are sent, don't choke on the params[:filter][:foo] below
-      params[:filter] ||= {}
-      # Remove blank params that sneak in the SELECT
-      params[:filter].each_pair { |k,v| params[:filter].delete(k) if v.blank? }
-    end
-    
-    def finalize_sphinx_results
-      # date range
-      # ThinkingSphinx apparently doesn't take logical operators, so we always need to provide it with an upper and lower bound for the date.
-      if @startdate.blank?
-        @startdate = AuditEvent.minimum(:created_at)
-      end
-      if @enddate.blank?
-        @enddate = Date.today
-      end
-
-      params[:range] = "created_at" unless (@startdate.blank? && @enddate.blank?)
-      params[:low] = Time.local(@startdate.year, @startdate.month, @startdate.day)
-      # high = last second of the day
-      params[:high] = Time.local(@enddate.year, @enddate.month, @enddate.day, 23, 59, 59)
-
-      # default to sort by descending date
-      params[:direction] ||= 'desc'
-
-      # search for our audits with a little help from our friend Sphinx-it!
-      @audits = AuditEvent.search(sphinxed_search_terms, sphinxed_search_conditions).compact
-
-      # Moving these params to save into controller, to be used on date "links"
-      @params_to_pass = {}
-      @params_to_pass[:filter] = {}
-      @params_to_pass[:filter][:ip_address] = params[:filter][:ip_address] unless params[:filter][:ip_address].blank?
-      @params_to_pass[:filter][:user_id] = params[:filter][:user_id] unless params[:filter][:user_id].blank?
-      @params_to_pass[:event_type] = params[:event_type] unless params[:event_type].blank?
-      @params_to_pass[:query] = params[:query] unless params[:query].blank?
-      @params_to_pass[:direction] = params[:direction] unless params[:direction].blank?
-      @params_to_pass[:startdate] = params[:startdate] unless params[:startdate].blank?
-      @params_to_pass[:enddate] = params[:enddate] unless params[:enddate].blank?
-      @params_to_pass[:filter][:auditable_type] = params[:filter][:auditable_type] unless params[:filter][:auditable_type].blank?
-      @params_to_pass[:filter][:auditable_id] = params[:filter][:auditable_id] unless params[:filter][:auditable_id].blank?
-
-
-      @params_to_pass_for_previous_day = { :startdate => @previous_date.strftime('%F') }.to_param unless @previous_date.nil?
-      @params_to_pass_for_next_day = { :startdate => @next_date.strftime('%F') }.to_param unless @next_date.nil?
-    end
   
 end
