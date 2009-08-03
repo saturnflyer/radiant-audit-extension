0fe62be50877841d26c1705f9b8cfa7f45d50799
Josh French <josh@digitalpulp.com>
2009-08-03 16:13:40 -0400

    Cruft

--- app/models/audit_observer.rb
+++ app/models/audit_observer.rb
@@ -11,20 +11,6 @@ class AuditObserver < ActiveRecord::Observer
 
   def before_update(model)
     audit :item => model, :user => @@current_user, :ip => @@current_ip, :type => :update
-
-    # allow an audit event to be chained on after the update event fires.
-    # define the OBSERVABLE_FIELDS you want to create custom AuditEvent messages for in your class
-    # (see page_extensions for an example) and the appropriate audit_event.
-    if defined?(model.class::OBSERVABLE_FIELDS) && model.class::OBSERVABLE_FIELDS.any?
-      model.class::OBSERVABLE_FIELDS.each do |field|
-        # check to see if the field you're interested in has changed using ActiveRecord's dirty methods
-        if model.send "#{field.to_s}_changed?"
-          # gsub out '_id' in the field to make the log message prettier (i.e. Status Changed vs Status ID Changed)
-          audit :item => model, :user => @@current_user, :ip => @@current_ip, :type => "#{field.to_s.gsub(/_id$/, '')}_change".to_sym
-        end
-      end
-    end
-
   end
   
   def after_destroy(model)
--- lib/audit/page_extensions.rb
+++ lib/audit/page_extensions.rb
@@ -2,11 +2,6 @@ module Audit
   module PageExtensions
 
     def self.included(base)
-      # add to this if you want to fire off separate AuditEvents when particular fields are
-      # updated-- i.e. add :status_id to observe when the page's status changes.
-      # see audit_event :status_change below for how to handle such an event
-      base.const_set("OBSERVABLE_FIELDS", [])
-
       base.class_eval do
         extend Auditable
         extend Auditor
@@ -22,25 +17,8 @@ module Audit
           log_message
         end
         
-        # separate event for logging page status changes- fired after page :update if the status has changed.
-        audit_event :status_change do |event|
-          oldstatus = Status.find_all.reject{|x| x.id != event.auditable.status_id_was}.first.name
-          log_message = "#{event.user_link} changed the status of " + link_to(event.auditable.title, event.auditable_path)
-          log_message += " from #{oldstatus}" unless oldstatus.blank?
-          log_message += " to #{event.auditable.status.name}"
-          log_message += " (" + link_to("link", edit_admin_page_path(event.auditable.id)) + ")"
-        end
-        
         audit_event :destroy do |event|
           "#{event.user_link} deleted " + link_to(event.auditable.title, event.auditable_path)
-        end
-
-        def self.updated_fields(updatables, auditable)
-          updated_fields = auditable.parts.inject(super||[]) do |updates,part|
-            updates << part.name if part.changed?
-            updates
-          end
-          updated_fields.any? ? updated_fields : nil
         end
       end
     end
