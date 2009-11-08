9680288e72b86071ddc9fa69ed406f7ef02da1f6
Josh French <josh@digitalpulp.com>
2009-11-08 15:19:07 -0500

    Kaizen

--- app/helpers/admin/audits_helper.rb
+++ app/helpers/admin/audits_helper.rb
@@ -1,12 +1,7 @@
 module Admin::AuditsHelper
   def browse_by_date_filters_set?
     # have any filters been set on the browse_by_date form?
-    not [params[:ip], params[:user], params[:event_type], params[:log]].all?(&:blank?)
-  end
-  
-  def custom_report_filters_set?
-    # have any filters been set on the custom_report form?
-    not [params[:before], params[:after], params[:ip], params[:user], params[:auditable_type], params[:auditable_id], params[:event_type], params[:log]].all? &:blank?
+    [params[:ip], params[:user], params[:event_type], params[:log], params[:auditable_id], params[:auditable_type]].compact.any?
   end
 
   def reverse_direction
@@ -18,11 +13,7 @@ module Admin::AuditsHelper
   end
   
   def event_user_name(event)
-    if event.user.nil?
-      event.log_message.match(/^File\sSystem/) ? "File System" : "(Unknown)"
-    else
-      event.user.login
-    end
+    event.user.try(:login)
   end
 
   def item_link
