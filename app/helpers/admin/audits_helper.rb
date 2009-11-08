a681c35eaefbd3f599b2d82a5a32fb5edf0cdbe7
Josh French <josh@digitalpulp.com>
2009-11-08 16:42:17 -0500

    Cruft

--- app/helpers/admin/audits_helper.rb
+++ app/helpers/admin/audits_helper.rb
@@ -24,16 +24,4 @@ module Admin::AuditsHelper
     event.user.try(:login) || '(Unknown)'
   end
 
-  def item_link
-    case @item
-    when Page : link_to @item.title, edit_admin_page_path(@item)
-    when Snippet : link_to @item.name, edit_admin_snippet_path(@item)
-    when Layout : link_to @item.name, edit_admin_layout_path(@item)
-    when User : link_to @item.login, edit_admin_user_path(@item)
-    when nil
-      if params[:auditable_type] and params[:auditable_id] and AuditEvent.find_by_auditable_type_and_auditable_id(params[:auditable_type], params[:auditable_id])
-        "#{(h params[:auditable_type]).camelcase} ##{h params[:auditable_id]} (deleted)"
-      end
-    end
-  end
 end
--- spec/helpers/admin/audits_helper_spec.rb
+++ spec/helpers/admin/audits_helper_spec.rb
@@ -35,27 +35,4 @@ describe Admin::AuditsHelper do
       filters_set?.should be_true
     end
   end
-
-  describe "item_link" do
-    it "should be empty if @item is not assigned" do
-      item_link.should be_blank
-    end
-
-    it "should be a page link if @item is a page" do
-      @item = pages(:first)
-      item_link.should =~ /a href=".+"/
-      item_link.should =~ /#{@item.title}/
-    end
-
-    it "should not be linked if @item is of an unknown class" do
-      @item = String.new
-      item_link.should be_blank
-    end
-
-    it "should say (deleted) if object does not exist but we have logs" do
-      @params = { :auditable_type => 'Page', :auditable_id => page_id(:first) }
-      pages(:first).destroy
-      item_link.should == "Page ##{page_id(:first)} (deleted)"
-    end
-  end
 end
