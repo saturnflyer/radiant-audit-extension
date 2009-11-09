1e0772df5650427235ebd85341163afde1904b0e
Alex Wallace <alex@digitalpulp.com>
2009-11-09 10:37:09 -0500

    use cleaner overlabel script

--- public/javascripts/admin/audit.js
+++ public/javascripts/admin/audit.js
@@ -1,6 +1,5 @@
 // Subclass DatePicker behavior to override the widget positioning and perform
 // as an auto-submit field.
-
 var AuditDatePicker = Behavior.create(DatePicker, {
 	_positionWidget : function() {
 		var offset = this.element.cumulativeOffset();
@@ -14,15 +13,50 @@ var AuditDatePicker = Behavior.create(DatePicker, {
 		this.element.form.submit();
 	}
 });
-
 // Steal the default CONIFG from the original DatePicker
-
 AuditDatePicker.CONFIG = DatePicker.CONFIG;
 
-// And add our behaviors
+// Generic Overlabel for in-field label
+var OverLabel = Behavior.create({
+	initialize : function(){
+		this.element.hide();
+		this.overlabel = this.element.innerHTML;
+		this.input = $(this.element.htmlFor);
+		if (this.overLabelIsWriteable)
+			this.initializeOverLabel();
+		this.element.up('form').observe("submit", this.clearOverLabelValue.bind(this));
+	},
+	initializeOverLabel : function(){
+		this.input.observe('click', this.overLabelFocusHandler.bindAsEventListener(this)).value = this.overlabel;
+	},
+	overLabelFocusHandler : function(){
+		if (this.overLabelIsWriteable) {
+			this.input_blur_handler = this.overLabelBlurHandler.bindAsEventListener(this);
+			this.input.observe('blur', this.input_blur_handler).clear();
+		}
+	},
+	overLabelBlurHandler : function(){
+		Event.stopObserving(this.input, 'blur', this.input_blur_handler);
+		if ($F(this.input).empty())
+			this.replaceOverLabel();
+	},
+	replaceOverLabel : function(){
+		this.input.value = this.overlabel;
+	},
+	overLabelIsWriteable : function(){
+		var val = $F(this.input);
+		return val == "" || val == this.overlabel;
+	},
+	clearOverLabelValue : function(){
+		if (this.input.value == this.overlabel)
+			this.input.clear();
+	}
+});
 
+// And add our behaviors
 Event.addBehavior({ 
-  ".AuditDatePicker" : AuditDatePicker
+  ".AuditDatePicker" : AuditDatePicker,
+	".OverLabel" : OverLabel
 });
 
 document.observe("dom:loaded", function(){
@@ -41,11 +75,4 @@ document.observe("dom:loaded", function(){
 	metadata_toggle.observe("click", toggleFilteringOptions);
 	if (document.location.href.indexOf("Filter") > -1)
 		toggleFilteringOptions();
-	
-	var log = $("log");
-	var msg = "Log Message";
-	if (log && !log.value.length > 0 && !(log.value && log.value == msg)) {
-		log.value = msg;
-		log.observe("focus", function() { if (log.value == msg) log.value = ""; });
-	}
 });
